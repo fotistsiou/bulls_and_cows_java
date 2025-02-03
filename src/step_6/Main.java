0365f6d90f213048c23e5a610618e85a3d2d8527
fotistsiou <fotis.tsioumas@outlook.com>
2025-02-03 10:07:02 +0200

    Update Steps

--- src/step_6/Main.java
+++ src/step_6/Main.java
@@ -1,4 +1,27 @@
 package step_6;
 
+/**
+ * New level
+ * ---------
+ * Description
+ * Some players need a challenge, so let's make the secret code in the game harder to guess. You should add support for
+ * more than 10 symbols by including letters too. Now, the secret code can contain the 10 numbers 0-9 and the 26
+ * lowercase Latin characters a-z. So, the new maximum length for the secret code is 36.
+ * You should also implement a system where users can select the number of possible symbols that can be used to create
+ * the secret code. So you should request input twice: once for the secret code length and once for the number of
+ * possible symbols in it. Note that the length of the secret code may not match the number of possible symbols in it.
+ * Therefore, it is important to verify that the secret code length is not greater than the number of possible symbols
+ * as the integers in secret code have to be unique.
+ * Also, since the secret code is not a number anymore, allow the symbol 0 as the first character in a secret code.
+ * ---------
+ * Objectives
+ * In this step, your program should:
+ * 1. Ask for the length of the secret code.
+ * 2. Ask for the range of possible symbols in the secret code.
+ * 3. Generate a secret code using numbers and characters. This time, you should also print the secret code using *
+ *    characters and print which symbols were used to generate the secret code.
+ * 4. Function as a fully playable game.
+ */
+
 public class Main {
 }
--- src/step_7/Main.java
+++ src/step_7/Main.java
@@ -1,4 +1,23 @@
 package step_7;
 
+/**
+ * Error!
+ * ------
+ * Description
+ * There are many possibilities for errors. What if someone enters an answer of the wrong length? Or if the number of
+ * possible symbols is less than the length of the code? What if the answer contains invalid symbols? The game may crash
+ * before the secret number is guessed!
+ * Let's handle errors like this. At this point, you can implement this without the try-catch construction. Use the
+ * following rule of thumb: if you can avoid exception-based logic, do so! If you use exceptions in normal situations,
+ * how would you deal with unusual (exceptional) situations? Now it may not seem that important, but when you need to
+ * find errors in more complex programs, this makes a difference.
+ * ------
+ * Objectives
+ * In this stage, your program should:
+ * 1. Handle incorrect input.
+ * 2. Print an error message that contains the word error. After that, don't ask for the numbers again, just finish the
+ *    program.
+ */
+
 public class Main {
 }
