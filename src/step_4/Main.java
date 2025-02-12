f7e7a22212096268a3d902e2eedadf2ff4b2c8c4
fotistsiou <fotis.tsioumas@outlook.com>
2025-02-12 10:55:07 +0200

    Step 4

--- src/step_4/Main.java
+++ src/step_4/Main.java
@@ -20,5 +20,107 @@ package step_4;
  * 4. If the secret code has been guessed, the program stops; otherwise, return to the second step.
  */
 
+import java.util.Scanner;
+
 public class Main {
+    public static String secretCode;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please, enter the secret code's length:");
+        int length = scanner.nextInt();
+        scanner.nextLine();
+        if (length > 10) {
+            System.out.printf(
+                    "Error: can't generate a secret number with a length of %d because there aren't enough unique digits.",
+                    length
+            );
+            scanner.close();
+            return;
+        }
+        Main.secretCode = Main.generateRandomCode(length);
+
+        System.out.println("Okay, let's start a game!");
+        Main.playGame(Main.secretCode, scanner);
+
+        scanner.close();
+    }
+
+    static String generateRandomCode(int length) {
+        char[] secretCode = new char[length];
+        int counter = 0;
+
+        while (counter < length) {
+            long pseudoRandomNumber = System.nanoTime();
+            StringBuilder stringBuilder = new StringBuilder(String.valueOf(pseudoRandomNumber)).reverse();
+            for (char c : stringBuilder.toString().toCharArray()) {
+                if (counter == 0) {
+                    if (c == '0') {
+                        continue;
+                    }
+                    secretCode[counter] = c;
+                    counter++;
+                }
+                if (counter == secretCode.length) {
+                    break;
+                }
+                if (Main.isUnique(secretCode, c, counter)) {
+                    secretCode[counter] = c;
+                    counter++;
+                }
+            }
+        }
+
+        return String.valueOf(secretCode);
+    }
+
+    static boolean isUnique(char[] secretCode, char c, int counter) {
+        for (int i = 0; i < counter; i++) {
+            if (secretCode[i] == c) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static void playGame(String secretCode, Scanner scanner) {
+        char[] secretCodeArray = secretCode.toCharArray();
+        int bulls = 0;
+        int round = 1;
+        while (bulls < secretCodeArray.length) {
+            System.out.printf("Turn %d:%n", round);
+            String guess = scanner.nextLine();
+            char[] guessToCharArray = guess.toCharArray();
+            int cows = 0;
+            bulls = 0;
+            for (int i = 0; i < guessToCharArray.length; i++) {
+                for (int j = 0; j < secretCodeArray.length; j++) {
+                    if (guessToCharArray[i] == secretCodeArray[j] && i == j) {
+                        bulls++;
+                        continue;
+                    }
+                    if (guessToCharArray[i] == secretCodeArray[j]) {
+                        cows++;
+                    }
+                }
+            }
+            if (bulls == 0 && cows == 0) {
+                System.out.println("Grade: None.");
+            } else if (bulls == 0) {
+                String cowsText = cows == 1 ? "cow" : "cows";
+                System.out.println("Grade: " + cows + " " + cowsText);
+            } else if (cows == 0) {
+                String bullsText = bulls == 1 ? "bull" : "bulls";
+                System.out.println("Grade: " + bulls + " " + bullsText);
+            } else {
+                String cowsText = cows == 1 ? "cow" : "cows";
+                String bullsText = bulls == 1 ? "bull" : "bulls";
+                System.out.println("Grade: " + bulls + " " + bullsText + " " + cows + " " + cowsText);
+            }
+            round++;
+        }
+        System.out.println("Congratulations! You guessed the secret code.");
+    }
 }
+
